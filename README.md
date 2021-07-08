7c99b5ac4573842cab96e27e878379f4ca96219e
lebhoryi <lebhoryi@gmail.com>
2021-07-08 16:21:19 +0800

    add v0.2.1

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 [TOC]
 
-| Version | Date       | Description                                                  | Other |
-| ------- | ---------- | ------------------------------------------------------------ | ----- |
-| v0.1.0  | 2021/07/06 | 火灾检测模型部署到 ART-PI 中                                 |       |
-| v0.2.0  | 2021/07/06 | 1. 新增 LCD 和 摄像头驱动;<br>2. 新增使用命令之后 logo 1s 显示 |       |
-|         |            |                                                              |       |
+| Version | Date       | Description                                                  | Other      |
+| ------- | ---------- | ------------------------------------------------------------ | ---------- |
+| v0.1.0  | 2021/07/06 | 火灾检测模型部署到 ART-PI 中                                 |            |
+| v0.2.0  | 2021/07/06 | 1. 新增 LCD 和 摄像头驱动;<br>2. 新增使用命令之后 logo 1s 显示 |            |
+| v0.2.1  | 2021/07/07 | 新增 c 代码版本的尺度转换，输入 320x240 转成 64x64           | 双线性插值 |
 
 怎么把生成 ART-PI，怎么使用 RT-AK，怎么使用 RT-Thread Sdutio 编译代码，请看下面这个链接：
 
@@ -111,11 +111,15 @@ uint8_t *out = (uint8_t *)rt_ai_output(model, 0);
 
 开机屏幕是白色的，
 
-当在终端输入 fire_app 之后会有 1s 的 logo 显示，然后黑屏。之后终端输出推理结果。
+当在终端输入 fire_app 之后会有 1s 的 logo 显示，然后显示输入图片和结果。之后终端输出推理结果。
+
+因为模型的输入是 320x240 大小的图片经过尺度缩放（双线性插值算法）之后获得的，所以跟之前的结果稍微有点出入。
 
 ![image](https://git.rt-thread.com/research/edge-ai-group/edge-ai/uploads/2a07cab8ad365f5cd4477cbde603241e/image.png)
 
-![image-20210706142543669](https://gitee.com/lebhoryi/PicGoPictureBed/raw/master/img/20210706142546.png)
+![image-20210708161905392](https://gitee.com/lebhoryi/PicGoPictureBed/raw/master/img/20210708161905.png)
+
+![image-20210708161623701](C:\Users\12813\AppData\Roaming\Typora\typora-user-images\image-20210708161623701.png)
 
 ---
 
--- applications/fire_detection.c
+++ applications/fire_detection.c
@@ -9,52 +9,115 @@
  */
 #include "drv_spi_ili9488.h"  // spi lcd driver
 #include <lcd_spi_port.h>  // lcd ports
+
+/* fire detection */
 #include <rt_ai_fire_model.h>
 #include <rt_ai.h>
 #include <rt_ai_log.h>
-
-/* fire detection */
 #include <logo.h>
 #include <test.h>
 
 struct rt_event ov2640_event;
 
-int ai_run_complete_flag = 0;
 void ai_run_complete(void *arg){
     *(int*)arg = 1;
 }
+
+void bilinera_interpolation(rt_uint8_t *in_array, short height, short width,
+                            rt_uint8_t *out_array, short out_height, short out_width);
 
 int fire_app(void){
     lcd_show_image(0, 0, 320, 240, LOGO);
     lcd_show_string(90, 140, 16, "Hello RT-Thread!");
     lcd_show_string(90, 156, 16, "Demo: Fire Detection");
-    rt_thread_mdelay(1000);
-    lcd_clear(BLACK);
+    lcd_show_image(0, 0, 320, 240, TEST);
 
     rt_err_t result = RT_EOK;
-    static rt_ai_t model = NULL;
-    rt_ai_buffer_t *work_buffer = rt_malloc(RT_AI_FIRE_WORK_BUFFER_BYTES+RT_AI_FIRE_IN_TOTAL_SIZE_BYTES+RT_AI_FIRE_OUT_TOTAL_SIZE_BYTES);
+    int ai_run_complete_flag = 0;
 
-    // find a registered model handle
+    /* find a registered model handle */
+    static rt_ai_t model = NULL;
     model = rt_ai_find(RT_AI_FIRE_MODEL_NAME);
     if(!model) {rt_kprintf("ai model find err\r\n"); return -1;}
 
-    // init the model and allocate memory
-    result = rt_ai_init(model, work_buffer);
-    if (result != 0) {rt_kprintf("ai init err\r\n"); return -1;}
 
-    // prepare input data
-    rt_memcpy(model->input[0], TEST, RT_AI_FIRE_IN_1_SIZE_BYTES);
+#if 1
+    /* init the model and allocate memory */
+    rt_ai_buffer_t *work_buffer = rt_malloc(RT_AI_FIRE_WORK_BUFFER_BYTES+RT_AI_FIRE_IN_TOTAL_SIZE_BYTES+RT_AI_FIRE_OUT_TOTAL_SIZE_BYTES);
+    result = rt_ai_init(model, work_buffer);
+    if (result != 0) {rt_kprintf("ai model init err\r\n"); return -1;}
+
+    /* prepare input data */
+    rt_ai_buffer_t *input_image = rt_malloc(RT_AI_FIRE_IN_1_SIZE_BYTES);
+    if (!input_image) {rt_kprintf("malloc input memory err\n"); return -1;}
+
+    // resize
+    bilinera_interpolation(TEST, 240, 320, input_image, 64, 64);
+
+    rt_memcpy(model->input[0], input_image, RT_AI_FIRE_IN_1_SIZE_BYTES);
+
+    /* run ai model */
     result = rt_ai_run(model, ai_run_complete, &ai_run_complete_flag);
     if (result != 0) {rt_kprintf("ai model run err\r\n"); return -1;}
 
-    // get output and post-process the output
+    /* get output and post-process the output */
+    uint8_t *pred;
     if(ai_run_complete_flag){
-        uint8_t *out = (uint8_t *)rt_ai_output(model, 0);
-        rt_kprintf("pred: %d %d\n", out[0], out[1]);
+        pred = (uint8_t *)rt_ai_output(model, 0);
+        rt_kprintf("prediction: %d %d\n", pred[0], pred[1]);
         // AI_LOG("Prediction: %d\n", prediction);
     }
     rt_free(work_buffer);
+
+
+
+    if (pred[0] > 200)
+        lcd_show_string(20, 20, 16, "Fire");
+    else
+        lcd_show_string(20, 20, 16, "No Fire");
+#endif
     return 0;
 }
 MSH_CMD_EXPORT(fire_app,fire demo);
+//INIT_COMPONENT_EXPORT(fire_app);
+
+
+int is_in_array(short x, short y, short height, short width)
+{
+    if (x >= 0 && x < width && y >= 0 && y < height)
+        return 1;
+    else
+        return 0;
+}
+
+
+void bilinera_interpolation(rt_uint8_t* in_array, short height, short width,
+                            rt_uint8_t* out_array, short out_height, short out_width)
+{
+    double h_times = (double)out_height / (double)height,
+           w_times = (double)out_width / (double)width;
+    short  x1, y1, x2, y2, f11, f12, f21, f22;
+    double x, y;
+
+    for (int i = 0; i < out_height; i++){
+        for (int j = 0; j < out_width*3; j=j+3){
+            for (int k =0; k <3; k++){
+                x = j / w_times + k;
+                y = i / h_times;
+
+                x1 = (short)(x - 3);
+                x2 = (short)(x + 3);
+                y1 = (short)(y + 1);
+                y2 = (short)(y - 1);
+                f11 = is_in_array(x1, y1, height, width*3) ? in_array[y1*width*3+x1] : 0;
+                f12 = is_in_array(x1, y2, height, width*3) ? in_array[y2*width*3+x1] : 0;
+                f21 = is_in_array(x2, y1, height, width*3) ? in_array[y1*width*3+x2] : 0;
+                f22 = is_in_array(x2, y2, height, width*3) ? in_array[y2*width*3+x2] : 0;
+                out_array[i*out_width*3+j+k] = (rt_uint8_t)(((f11 * (x2 - x) * (y2 - y)) +
+                                           (f21 * (x - x1) * (y2 - y)) +
+                                           (f12 * (x2 - x) * (y - y1)) +
+                                           (f22 * (x - x1) * (y - y1))) / ((x2 - x1) * (y2 - y1)));
+            }
+        }
+    }
+}
